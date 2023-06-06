63c100950d9e08a8c846963c72be55a69061e606
Van Fernandes <vanilson.fernandes@calindra.com.br>
2023-06-06 07:26:54 -0300

    feat: tests

---
+++ app/src/test/java/com/example/tiptime/TipCalculatorTest.kt
@@ -0,0 +1,20 @@
+package com.example.tiptime
+
+import java.text.NumberFormat
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TipCalculatorTest {
+
+    @Test
+    fun calculateTip_20PercentNoRoundUp(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, roundUp = false)
+
+        assertEquals(expectedTip, actualTip)
+
+    }
+}
